f14839d85fa59a3fc8617f5df0eebd514ca6acd5
Allison MacQueen <allimacq@gmail.com>
2020-09-20 23:38:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,28 @@
 # Add your code here
+class Dog 
+  
+  @@all =  [ ]
+  
+  def initialize (name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all 
+    @@all
+  end
+  
+  def self.clear_all
+    @@all = [ ]
+  end
+  
+  def self.print_all
+    @@all 
+  end
+  
+  
+  def name
+    @name
+  end
+  
+end
